a11f14e66bcfc417f7cb81844ea5badf3bfa9bb5
Shahin Sarker <141581883+Shahin4292@users.noreply.github.com>
2024-11-29 09:06:31 +0600

    add AppColors

---
+++ lib/backend/fetch_news.dart
@@ -0,0 +1,14 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:tech_newz/utils/key.dart';
+
+Future<List> fetchNews() async {
+  final response = await http.get(Uri.parse(
+      'https://newsapi.org/v2/top-headlines?country=us&category=technology&pageSize=100&apiKey=' +
+          Key.key +
+          '&q='));
+  Map result = jsonDecode(response.body);
+  print('Fetched');
+  return (result['articles']);
+}
